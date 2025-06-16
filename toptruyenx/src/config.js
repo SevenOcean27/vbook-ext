7889fb8dd490235bfa50e308182acdf5f1670d0d
SevenOcean27 <68941972+SevenOcean27@users.noreply.github.com>
2025-06-16 20:15:46 +0700

    update Toptruyen

--- toptruyenx/src/config.js
+++ toptruyenx/src/config.js
@@ -1,5 +1,5 @@
-let BASE_URL = 'https://www.toptruyentv7.pro';
-let BASE_URL1 = 'https://toptruyentv6.pro'
+let BASE_URL = 'https://www.toptruyentv8.com';
+let BASE_URL1 = 'https://toptruyentv7.pro'
 try {
     if (CONFIG_URL) {
         BASE_URL = CONFIG_URL;
