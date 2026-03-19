a7bf2f2e06188a4aca944c7186ad374aa1b57644
SevenOcean27 <68941972+SevenOcean27@users.noreply.github.com>
2026-03-20 00:54:33 +0700

    Update Toptruyen

--- toptruyenx/src/config.js
+++ toptruyenx/src/config.js
@@ -1,5 +1,5 @@
-let BASE_URL = 'https://www.toptruyenss.com';
-let BASE_URL1 = 'https://toptruyenss.com'
+let BASE_URL = 'https://www.toptruyenssz.com';
+let BASE_URL1 = 'https://toptruyenssz.com'
 try {
     if (CONFIG_URL) {
         BASE_URL = CONFIG_URL;
